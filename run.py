9804f37d5d73d64ef4c646dd4210e798aa0e51f8
Ahmad_Choudhry <63918120+ahmad-zaheer@users.noreply.github.com>
2023-12-04 09:44:40 -0500

    updates

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 from dqn_agent import DQNAgent
-from tetris import Board
+import tetris
+from tetris import Piece, Board, make_random_piece, possible_states
 from datetime import datetime
 from statistics import mean, median
 import random
 from logs import CustomTensorBoard
 from tqdm import tqdm
-        
 
-# Run dqn with Board
 def dqn():
     env = Board()
-    episodes = 2000
+    piece = make_random_piece(env)
+    episodes = 20
     max_steps = None
     epsilon_stop_episode = 1500
     mem_size = 20000
@@ -30,54 +30,39 @@ def dqn():
                      epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
                      discount=discount, replay_start_size=replay_start_size)
 
-    log_dir = f'logs/tetris-nn={str(n_neurons)}-mem={mem_size}-bs={batch_size}-e={epochs}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    log_dir = f'logs/tetris-nn={"-".join(map(str, n_neurons))}-mem={mem_size}-bs={batch_size}-e={epochs}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
     log = CustomTensorBoard(log_dir=log_dir)
 
     scores = []
 
-    for episode in tqdm(range(episodes)):
+    for episode in tqdm(range(episodes), desc="Training Episodes"):
         current_state = env.reset()
+        print(f'Initial State: {current_state}')
         done = False
         steps = 0
+        render = render_every and episode % render_every == 0
 
-        if render_every and episode % render_every == 0:
-            render = True
-        else:
-            render = False
-
-        # Game
         while not done and (not max_steps or steps < max_steps):
             next_states = env.get_next_states()
             best_state = agent.best_state(next_states.values())
-            
-            best_action = None
-            for action, state in next_states.items():
-                if state == best_state:
-                    best_action = action
-                    break
+            best_action = next((action for action, state in next_states.items() if state == best_state), None)
 
-            reward, done = env.play(best_action[0], best_action[1], render=render,
-                                    render_delay=render_delay)
-            
-            agent.add_to_memory(current_state, next_states[best_action], reward, done)
-            current_state = next_states[best_action]
-            steps += 1
+            if best_action is not None:
+                reward, done = env.play(best_action[0], best_action[1], render=render, render_delay=render_delay)
+                agent.add_to_memory(current_state, next_states[best_action], reward, done)
+                current_state = next_states[best_action]
+                steps += 1
 
         scores.append(env.get_game_score())
 
-        # Train
         if episode % train_every == 0:
             agent.train(batch_size=batch_size, epochs=epochs)
 
-        # Logs
         if log_every and episode and episode % log_every == 0:
             avg_score = mean(scores[-log_every:])
             min_score = min(scores[-log_every:])
             max_score = max(scores[-log_every:])
-
-            log.log(episode, avg_score=avg_score, min_score=min_score,
-                    max_score=max_score)
-
+            log.log(episode, avg_score=avg_score, min_score=min_score, max_score=max_score)
 
 if __name__ == "__main__":
     dqn()
